37b19c2be2f3bf2a3d756e67716a9d973916c2e9
Amish Shah <Amish Shah>
2017-03-27 22:50:44 -0400

    #4 Made more generic where intent can be present in any class

--- AppParser/src/main/java/Prefetch/TraceIntentCalls.java
+++ AppParser/src/main/java/Prefetch/TraceIntentCalls.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +12,7 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -22,40 +24,31 @@ public class TraceIntentCalls {
 	static String prefetchInitiator;
 	static String prefetchReceiver;
 	static List<String> networkresources = new ArrayList<String>();
-
+	static String filename;
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		filename = "MainActivity";
 		FileInputStream in = new FileInputStream(
-				"C:/Users/shaha/Documents/PrefetchProcrastinatorAndroid/VolleyProcrastrinate/app/src/main/java/com/example/tau/volleyprocrastrinate/MainActivity.java");
+				"C:/Users/shaha/Documents/PrefetchProcrastinatorAndroid/VolleyProcrastrinate/app/src/main/java/com/example/tau/volleyprocrastrinate/"+filename+".java");
 		// parse it
 		CompilationUnit cu = JavaParser.parse(in);
 		new MethodChangerVisitor().visit(cu, null);
 		// System.out.println(cu);
+		System.out.println(prefetchInitiator+","+prefetchReceiver+","+networkresources);
 	}
 
 	private static class MethodChangerVisitor extends VoidVisitorAdapter<Void> {
 		@Override
 		public void visit(MethodDeclaration n, Void arg) {
-			// change the name of the method to upper case
-			if (n.getNameAsString().equals("onPostExecute")) {
+			// Check whether a method contains instantiation of Intent
+			if (n.toString().contains("new Intent")) {
 				/*
 				 * n.addParameter("int", "value"); NodeList a =
 				 * n.getParameters(); a.getParentNode();
 				 */
+
 				Node methodBlock = (n.getChildNodes().get(n.getChildNodes().size() - 1));
-				if (methodBlock.getChildNodes().get(0) instanceof IfStmt) {
-					Node ifStatement = methodBlock.getChildNodes().get(0);
-					Node ifStatementBlock = ifStatement.getChildNodes().get(ifStatement.getChildNodes().size() - 1);
-
-					int count = 0;
-					while (count < ifStatementBlock.getChildNodes().size()) {
-						Node statement = ifStatementBlock.getChildNodes().get(count);
-						fetchValues(statement);
-						count++;
-
-					}
-
-				} else {
+				 
 					methodBlock = (n.getChildNodes().get(n.getChildNodes().size() - 1));
 					int count = 0;
 					while (count < methodBlock.getChildNodes().size()) {
@@ -64,7 +57,7 @@ public class TraceIntentCalls {
 						count++;
 					}
 
-				}
+				
 			}
 		}
 		public void fetchValues(Node statement){
@@ -78,10 +71,14 @@ public class TraceIntentCalls {
 				if (m.find()) {
 					String[] classes = new String[2];
 					classes = m.group(1).split(",");
-					classes[0] = classes[0].replaceAll("\\s", "").substring(0, classes[0].indexOf("."));
-					classes[1] = classes[1].replaceAll("\\s", "").substring(0, classes[1].indexOf("."));
-					prefetchInitiator = classes[0];
-					prefetchReceiver = classes[1];
+					// Context of current class can be passed as "this" or getApplicationContext
+					if ((classes[0].contains("this") && !classes[0].contains(".this"))  || classes[0].contains("getApplicationContext")){
+						prefetchInitiator = filename;
+					}
+					else{
+						prefetchInitiator = classes[0].replaceAll("\\s", "").substring(0, classes[0].indexOf("."));
+					}
+					prefetchReceiver = classes[1].substring(0, classes[1].indexOf("."));
 				}
 			}
 			
@@ -91,7 +88,6 @@ public class TraceIntentCalls {
 				Matcher m = p.matcher(statement.toString());
 				if (m.find()) {
 					networkresources.add(m.group(1).split(",")[1].replaceAll("\\s", ""));// Store the second result
-					System.out.println(networkresources);
 				}
 			
 			}
